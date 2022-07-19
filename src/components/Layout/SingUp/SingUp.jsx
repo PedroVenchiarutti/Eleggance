6e42f505e15a2355cd7690a70b809b3412679a85
BrenoOliveira <breno_2002_@outlook.com>
2022-07-19 12:54:02 -0300

    Structuring the canvas

--- src/components/Layout/SingUp/SingUp.jsx
+++ src/components/Layout/SingUp/SingUp.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import "./SingUp.scss";
 
-export const SingUp = () => {
+const SingUp = () => {
   return (
-    <div className="singup">
-      <h1>h1</h1>
+    <div className="container">
+      <div className="card-signUp">
+        <div>
+          <div className="text">
+            <h1>Sign In</h1>
+          </div>
+          <div className="icon"></div>
+          <div className="card-one">
+            <input type="text" className="input" />
+            <input type="password" className="input" />
+          </div>
+          <span className="forgot" type="button">
+            Forgot your password?
+          </span>
+        </div>
+      </div>
+
+      <div className="card-singIn">
+        <div className="tittlesSignIn">
+          <div className="a1">
+            <h1>Sign Up</h1>
+          </div>
+          <div className="a2">
+            <h3>Sign up here if you dont have account</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
