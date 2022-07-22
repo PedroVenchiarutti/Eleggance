f48322ad100661d5a166d86957115233db41ed63
BrenoOliveira <breno_2002_@outlook.com>
2022-07-22 12:39:18 -0300

    TaskBar, IconsHomePage

---
+++ src/components/Layout/HomePage/TaskBar.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import "./TaskBar.scss";
+
+export default (props) => {
+  return (
+    <div className="container-task-bar">
+      <div className="columns-task-bar">
+        <div className="b">
+          <div className="icon-task-bar">
+          <img src="\icons\iconmonstr-delivery-1-96.png" alt="ImgIconeFrete" />
+          </div>
+          <ul>
+            <li>
+              <div className="h4-titulo">
+                <h4>Frete Gratis</h4>
+              </div>
+            </li>
+            <li>
+              <div className="p-titulo">
+               <p> Para compras acima de R$ 200 em todo o site</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="b">
+          <div className="icon-task-bar">
+          <img src="\icons\icone-credit-card.png" alt="ImgIconeCard" />
+          </div>
+          <ul>
+            <li>
+            <div className="h4-titulo">
+                <h4>10x Sem Juros</h4>
+              </div>
+            </li>
+            <li>
+              <div className="p-titulo">
+                <p> Em todo o site e em todos os cartoes</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="b">
+          <div className="icon-task-bar">
+          <img src="\icons\icon-boleto.png" alt="ImgIconeCard" />
+          </div>
+          <ul>
+            <li>
+            <div className="h4-titulo">
+                <h4>5% de desconto</h4>
+              </div>
+            </li>
+            <li>
+              <div className="p-titulo">
+                <p>A vista no boleto ou Pix direto no carrinho</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="b">
+          <div className="icon-task-bar">
+          <img src="\icons\icon-lock.png" alt="ImgIconeCard" />
+          </div>
+          <ul>
+            <li>
+            <div className="h4-titulo">
+                <h4>Site 100% seguro</h4>
+              </div>
+            </li>
+            <li>
+              <div className="p-titulo">
+               <p> Compra segura, site com tecnologia SSL</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        </div>
+      </div>
+  );
+};
