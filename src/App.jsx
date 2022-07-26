fc68619348fa14e1170ae338edc3eb044856ad8f
Smooke09 <pedrolucas1014zz@gmail.com>
2022-07-25 21:16:00 -0300

    restructuring screens homePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import RegistrationForm from './pages/RegistrationForm/registrationForm';
-import HomePage from './pages/HomePage/HomePage';
-import FormSingUp from './pages/utils/FormValidation/Forms/FormSingUp/FormSingUp';
-import FormCadastro from './pages/utils/FormValidation/Forms/FormCadastro/FormCadastro';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegistrationForm from "./pages/RegistrationForm/registrationForm";
+import HomePage from "./pages/HomePage/HomePage";
+import FormSingUp from "./pages/FormSingUp/FormSingUp";
+import FormCadastro from "./pages/FormCadastro/FormCadastro";
 
 export const App = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/home" element={<HomePage />} />
-				<Route path="/cadastro" element={<FormCadastro />} />
-				<Route path="/login" element={<FormSingUp />} />
-				<Route path="/registration" element={<RegistrationForm />} />
-				<Route path="*" element={<HomePage to="/" replace />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/cadastro" element={<FormCadastro />} />
+        <Route path="/login" element={<FormSingUp />} />
+        <Route path="/registration" element={<RegistrationForm />} />
+        <Route path="*" element={<HomePage to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/SectionCarousel/index.jsx
+++ src/components/SectionCarousel/index.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from 'swiper';
+import React from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
 
 export const SectionCarousel = ({ images }) => {
-	return (
-		<Swiper
-			slidesPerView={1}
-			spaceBetween={30}
-			loop={true}
-			pagination={{
-				clickable: true,
-			}}
-			navigation={true}
-			modules={[Pagination, Navigation]}
-			className="mySwiper"
-		>
-			{images.map((image, i) => {
-				return (
-					<SwiperSlide key={i}>
-						<img src={image.url} alt="" />
-					</SwiperSlide>
-				);
-			})}
-		</Swiper>
-	);
+  return (
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      loop={true}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      className="mySwiper"
+    >
+      {images.map((image, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <img src={image.url} alt="" />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
