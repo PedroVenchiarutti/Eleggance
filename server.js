5ef2db9aeb938f71670745eb47c7015a3024edad
Smooke09 <pedrolucas1014zz@gmail.com>
2022-07-27 09:11:53 -0300

    ajust deploy heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { resolve } = require("path");
+const app = express();
+
+app.use("/", express.static(resolve(__dirname, "./build")));
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server is running on port 3000");
+});
