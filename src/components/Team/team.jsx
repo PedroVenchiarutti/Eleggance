584e7ce8888f0061e823dc6573d8a478ff4cceac
BrenoOliveira <breno_2002_@outlook.com>
2022-07-29 12:40:19 -0300

    bug fixeds in screen

--- src/components/Team/team.jsx
+++ src/components/Team/team.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../../pages/Footer/Footer";
 import "./team.scss";
 
 export default (props) => {
--- src/pages/Schedulling/schedulling.jsx
+++ src/pages/Schedulling/schedulling.jsx
@@ -1,23 +1,20 @@
 import React from "react";
-import "./schedulling.scss"
+import "./schedulling.scss";
 import { Navbar } from "../../components/Navbar";
 import Footer from "../Footer/Footer";
 import { HomeBanner } from "../../components/HomeBanner";
 import { images, shelfProducts, trendProducts } from "../../api/mock";
 import Team from "../../components/Team/team";
 
-export default props => {
-
-    return(
-        <div>
-             <header className="homepage">
+export default (props) => {
+  return (
+    <div>
+      <header className="homepage">
         <Navbar />
       </header>
       <HomeBanner images={images} />
       <Team />
       <Footer />
-        </div>
-
-
-    )
+    </div>
+  );
 }
