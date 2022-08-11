47ef4ac2f51f36380de2e724b21f8d9422130fdd
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-08-11 11:43:01 -0300

    using stat in form add products

--- src/components/ModalAddProducts/Index.jsx
+++ src/components/ModalAddProducts/Index.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ModalAddProduct() {
+  const [valor, setValor] = useState({
+    name: "testeeet",
+    price: 0,
+    descrition: "",
+    brand: "",
+    qtProduct: "",
+    urlImage: "",
+  });
   function modalToggle() {
     let modalAdd = document.getElementById("modalAddProducts");
     modalAdd.classList.toggle("open");
   }
+
+  console.log(valor);
   return (
     <div className="modalAddProducts" id="modalAddProducts">
       <div className="header-modal">
@@ -13,17 +23,51 @@ export default function ModalAddProduct() {
       </div>
       <div className="main-modal">
         <label>Nome Do Produto:</label>
-        <input maxLength={45} type="text" />
+        <input
+          maxLength={45}
+          type="text"
+          value={valor.name}
+          onChange={(e) => setValor({ ...valor, name: e.target.value })}
+        />
         <label>Valor:</label>
-        <input max={5} type="number" />
-        <label>Descrição</label>
-        <input maxLength={255} type="text" />
+        <input
+          type="number"
+          value={valor.price}
+          onchange={(e) => setValor({ ...valor, price: e.target.value })}
+        />
+        <label>Descrição:</label>
+        <input
+          maxLength={255}
+          type="text"
+          value={valor.descrition}
+          onchange={(e) => setValor({ ...valor, description: e.target.value })}
+        />
         <label>Categoria</label>
-        <input maxLength={45} type="text" />
+        <input
+          maxLength={45}
+          type="text"
+          value={valor.brand}
+          onchange={(e) => setValor({ ...valor, brand: e.target.value })}
+        />
         <label>Quantidade Disponível</label>
-        <input maxLength={3} type="number" />
+        <input
+          maxLength={3}
+          type="number"
+          value={valor.qtProduct}
+          onchange={(e) =>
+            setValor({
+              ...valor,
+              qtProduct: e.target.value,
+            })
+          }
+        />
         <label>Foto Do Produto</label>
-        <input type="file" className="inputPhoto" />
+        <input
+          type="file"
+          className="inputPhoto"
+          value={valor.urlImage}
+          onchange={(e) => setValor({ ...valor, urlImage: e.target.value })}
+        />
       </div>
       <div className="areaBtn">
         <div className="btn btnCadastrarProduto">Cadastrar Produto</div>
