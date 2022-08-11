9b8ad7bc18cb2848fcefbe79a05fe3fc568404cf
Smooke09 <pedrolucas1014zz@gmail.com>
2022-08-11 11:45:22 -0300

    refactoring methods

--- src/api/api.js
+++ src/api/api.js
@@ -1,24 +1,7 @@
 import axios from "axios";
 
-const apiGet = axios.create({
-  baseURL: "http://localhost:3333/",
-  method: "GET",
-});
-
-const apiPost = axios.create({
-  baseURL: "http://localhost:3333/",
-  method: "POST",
-  data: {},
-});
-
-/* const Api = axios.create({
-  baseURL: "http://localhost:3333/",
-  method: "GET",
-});
-
 const Api = axios.create({
   baseURL: "http://localhost:3333/",
-  method: "GET",
-}); */
+});
 
-export { apiGet, apiPost };
+export default Api;
