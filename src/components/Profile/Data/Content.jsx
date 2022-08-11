93ae0662aff6ecffb38a2b5e328369e88d4190d3
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-08-11 12:34:28 -0300

    Fix for CSS Data

--- src/components/Profile/Data/Content.jsx
+++ src/components/Profile/Data/Content.jsx
@@ -3,6 +3,7 @@ import ClientMenu from "../common/ClientMenu";
 import Data from "../../Data/Data"
 import '../Profile.scss'
 import Form from '../../Form/Form'
+import './Content.scss'
 
 export default (props) => {
 
@@ -11,63 +12,50 @@ export default (props) => {
     //     const number = document.getElementById("phoneNumber");
     //     const num = number.value
     //     function phoneMask(){
-        
+
     //     }
 
     //     phoneMask()
 
     // })
-    
 
-    return(
+    return (
+        <div className="profile-container">
+            <ClientMenu selected='dados' />
+            <div className="main-content">
+                <Data header='Meus dados cadastrais'>
+                    <Form className="form">
+                        <div className="formWritable">
+                            <label htmlFor="name">Nome Completo:</label>
+                            <input type="text" name="name" className="nameInput" />
+                            <div className="phoneAndGender">
+                                <span>
+                                    <label htmlFor="phone">Telefone:</label>
+                                    <input type="tel" name="phone" placeholder="13 9 1234-1234" id="phoneNumber" maxLength="13" />
+                                </span>
+                                <span>
+                                    <label htmlFor="gender">Sexo:</label>
+                                    <select name="gender" id="">
+                                        <option value="male">Masculino</option>
+                                        <option value="female">Feminino</option>
+                                        <option value="other">Outro</option>
+                                    </select>
+                                </span>
+                            </div>
+                            <br />
+                            <input type="submit" value="Salvar Alterações" className="submit" />
+                        </div>
+                        <div className="formReadOnly">
+                            <label htmlFor="cpf">CPF:</label>
+                            <input type="text" name="cpf" readOnly className="readOnly" value='props.cpf' />
 
-            <div className="profile-container">
-                <ClientMenu selected='dados'/>
-                <div className="main-content">
-                  <Data header='Meus dados cadastrais'>
-                    <Form >
-
+                            <label htmlFor="birth" >Data de nascimento:</label>
+                            <input type="text" name="birth" readOnly className="readOnly" value='props.birthdate' />
+                        </div>
                     </Form>
-
-
-
-
-
-                        <form action="" className="formData">
-                            <div className="formWritable">
-                                <label htmlFor="name">Nome Completo:</label>
-                                <input type="text" name="name" className="nameInput"/>
-                                <div className="phoneAndGender">
-                                    <span>
-                                        <label htmlFor="phone">Telefone:</label>
-                                        <input type="tel" name="phone" placeholder="13 9 1234-1234" id="phoneNumber" maxLength="13" />
-                                    </span>
-                                    <span>
-                                        <label htmlFor="gender">Sexo:</label>
-                                            {/* <input type="text" name="gender" className="gender"/> */}
-                                        <select name="gender" id="">
-                                            <option value="male">Masculino</option>
-                                            <option value="female">Feminino</option>
-                                            <option value="other">Outro</option>
-                                        </select>
-                                    </span>
-                                </div>
-                                    <br />
-                                <input type="submit" value="Salvar alterações" className="submit"/>
-                            </div>
-                            <div className="formReadOnly">
-                                <label htmlFor="cpf">CPF:</label>
-                                <input type="text" name="cpf" readOnly className="readOnly" value='props.cpf'/>
-
-                                <label htmlFor="birth" >Data de nascimento:</label>
-                                <input type="text" name="birth" readOnly className="readOnly" value='props.birthdate'/>
-                            </div>
-                        </form>
-                    </Data>
-    
-                </div>
+                </Data>
             </div>
-
+        </div>
     )
 
 }
