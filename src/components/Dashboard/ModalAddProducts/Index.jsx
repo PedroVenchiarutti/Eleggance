f37655e6c519037f258c19971be295e2fafa5fc8
MatheusSimoes13 <matheus.ruff@hotmail.com>
2022-08-16 11:38:38 -0300

    Dashboard create product modal

--- src/components/Dashboard/ModalAddProducts/Index.jsx
+++ src/components/Dashboard/ModalAddProducts/Index.jsx
@@ -31,61 +31,77 @@ export default function ModalAddProduct() {
         <h1>Cadastrar Produto</h1>
         <button onClick={modalToggle}>X</button>
       </div>
-      <div className="main-modal">
-        <label>Nome Do Produto:</label>
-        <input
-          maxLength={45}
-          type="text"
-          value={valor.name}
-          onChange={(e) => setValor({ ...valor, name: e.target.value })}
-        />
-        <label>Valor:</label>
-        <input
-          type="number"
-          value={valor.value}
-          onChange={(e) => setValor({ ...valor, price: e.target.value })}
-        />
-        <label>Descrição:</label>
-        <input
-          maxLength={255}
-          type="text"
-          value={valor.description}
-          onChange={(e) => setValor({ ...valor, description: e.target.value })}
-        />
-        <label>Categoria</label>
-        <input
-          maxLength={45}
-          type="text"
-          value={valor.brand}
-          onChange={(e) => setValor({ ...valor, brand: e.target.value })}
-        />
-        <label>Quantidade Disponível</label>
-        <input
-          maxLength={3}
-          type="number"
-          value={valor.qt}
-          onChange={(e) =>
-            setValor({
-              ...valor,
-              qt: e.target.value,
-            })
-          }
-        />
-        <label>Foto Do Produto</label>
-        <input
-          type="file"
-          className="inputPhoto"
-          value={valor.url_img}
-          onChange={(e) => setValor({ ...valor, url_img: e.target.value })}
-        />
-      </div>
-      <div className="areaBtn">
-        <button className="btn btnCadastrarProduto" onClick={postItem}>
-          Cadastrar Produto
-        </button>
-        <div className="btn btnCancelarCadastrarProduto" onClick={modalToggle}>
-          Cancelar
+      <div className="body-modal">
+        <div className="main-modal">
+          <label>Nome Do Produto:</label>
+          <input
+            maxLength={45}
+            type="text"
+            value={valor.name}
+            onChange={(e) => setValor({ ...valor, name: e.target.value })}
+          />
+          <label>Valor:</label>
+          <input
+            type="number"
+            value={valor.value}
+            onChange={(e) => setValor({ ...valor, price: e.target.value })}
+          />
+          <label>Descrição:</label>
+          <input
+            maxLength={255}
+            type="text"
+            value={valor.description}
+            onChange={(e) => setValor({ ...valor, description: e.target.value })}
+          />
+          <label>Categoria</label>
+          <input
+            maxLength={45}
+            type="text"
+            value={valor.brand}
+            onChange={(e) => setValor({ ...valor, brand: e.target.value })}
+          />
+          <label>Quantidade Disponível</label>
+          <input
+            maxLength={3}
+            type="number"
+            value={valor.qt}
+            onChange={(e) =>
+              setValor({
+                ...valor,
+                qt: e.target.value,
+              })
+            }
+          />
+          <label>Foto Do Produto</label>
+          <input
+            type="file"
+            className="inputPhoto"
+            value={valor.url_img}
+            onChange={(e) => setValor({ ...valor, url_img: e.target.value })}
+          />
         </div>
+        <div className="productVisualization">
+          <p>Visualização</p>
+          <div className="productAndInfo">
+            <img src="/test.png" alt="" />
+            <div className="productInfo">
+              <h4>Tratamento Único belkit</h4>
+              <h1>R$ 45,99</h1>
+              <p>10x de R$4,99 sem juros</p>
+              <p>Frete Grátis</p>
+              <h5>O tratamento único Belkit melhora a força e resistência do cabelo...</h5>
+            </div>
+          </div>
+          <div className="areaBtn">
+          <button className="btn btnCadastrarProduto" onClick={postItem}>
+            CADASTRAR
+          </button>
+          <div className="btn btnCancelarCadastrarProduto" onClick={modalToggle}>
+            CANCELAR
+          </div>
+        </div>
+        </div>
+
       </div>
     </div>
   );
