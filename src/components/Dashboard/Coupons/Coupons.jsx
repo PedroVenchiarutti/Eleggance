ff8ead0045ffe3f7d00c5def69ca25b51d88acd7
Douglas <douglasvitor.dev@gmail.com>
2022-08-17 09:28:00 -0300

    Setting responsiveness in table and admin coupon page

--- src/components/Dashboard/Coupons/Coupons.jsx
+++ src/components/Dashboard/Coupons/Coupons.jsx
@@ -8,14 +8,14 @@ import '../Dashboard.scss'
 import './Coupons.scss'
 
 export default () => {
-    const headerColumns = ["Código", "Valor mínimo", "Valor desconto", "Data de início", "Data de expiração", "Quantidade"];
-    const bodyRows = [["123-AB", "R$199,99", "20%", "02/08/2022", "12/08/2022", "40"],["123-CD", "R$249,99", "30%", "02/08/2022", "15/08/2022", "20"]];
+    const headerColumns = ["Código", "Valor mínimo", "Valor desconto", <th className="responsive-hide">Data de início</th>, <th className="responsive-hide">Data de expiração</th>, "Quantidade"];
+    const bodyRows = [["123-AB", "R$199,99", "20%", <td className="responsive-hide">02/08/2022</td>, <td className="responsive-hide">12/08/2022</td>, "40"],["123-CD", "R$249,99", "30%", <td className="responsive-hide">02/08/2022</td>, <td className="responsive-hide">12/08/2022</td>, "20"]];
 
     const [modalVisibility, setModalVisibility] = useState(false);
-    const toggleCreateModal = () => modalVisibility ? setModalVisibility(false) : setModalVisibility(true);
+    const toggleCreateModal = () => setModalVisibility(!modalVisibility);
 
     return (
-        <div className="dashboard-container">
+        <div className="content">
             <Table headerColumns={headerColumns} bodyRows={bodyRows} />
             <CouponModal showModal={modalVisibility} toggleVisibility={toggleCreateModal} />
 
--- src/components/Dashboard/Orders/Orders.jsx
+++ src/components/Dashboard/Orders/Orders.jsx
@@ -3,11 +3,11 @@ import Table from "../../Table/Table";
 import '../Dashboard.scss'
 
 export default () => {
-    const headerColumns = [<th>Produtos</th>, "Quantidade", "Valor", "Status pagamento", "Status entrega", "Código da venda"];
-    const pedidos = [["Batom cor rosê cremoso", "2", "R$99.99", "Pendente", "Pendente", "123"],["Batom cor verde cremoso", "2", "R$99.99", "Pendente", "Pendente", "1223"]];
+    const headerColumns = ["Produtos", "Quantidade", "Valor", <th className="responsive-hide">Status pagamento</th>, <th className="responsive-hide">Status entrega</th>, "Código da venda"];
+    const pedidos = [["Batom cor rosê cremoso", "2", "R$99.99", <td className="responsive-hide">Pendente</td>, <td className="responsive-hide">Pendente</td>, "123"], ["Batom cor verde cremoso", "2", "R$99.99", <td className="responsive-hide">Pendente</td>, <td className="responsive-hide">Pendente</td>, "1223"]];
 
     return (
-        <div className="dashboard-container">
+        <div className="content">
             <Table headerColumns={headerColumns} bodyRows={pedidos} />
         </div>
     )
