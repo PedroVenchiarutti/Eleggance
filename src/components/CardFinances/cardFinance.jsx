48c8af1768400dad3c83766382ec1fd3c870aad9
BrenoOliveira <breno_2002_@outlook.com>
2022-08-25 12:28:00 -0300

    I finished the search bar functionality

--- src/components/CardFinances/cardFinance.jsx
+++ src/components/CardFinances/cardFinance.jsx
@@ -1,24 +1,45 @@
 import React from "react";
-import "../CardFinances/cardFinance.scss"
+import "../CardFinances/cardFinance.scss";
+import { NavLink } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 
 export default (props) => {
+  const { data } = useFetch(`api/public/products/pages/1`);
+  let pedidos = data.length;
+
   return (
     <div className="cards-finances">
       <div className="div-card-finances">
-        <h3> Reservas </h3>
-        <h2> 0 </h2>
+        <NavLink to={"/admin/reservas"}>
+          <div className="link-financas">
+            <h3> Reservas </h3>
+            <h2> 0 </h2>
+          </div>
+        </NavLink>
       </div>
       <div className="div-card-finances">
-        <h3> Cupons gerados </h3>
-        <h2> 0 </h2>
+        <NavLink to={"/admin/cupons"}>
+          <div className="link-financas">
+            <h3> Cupons gerados </h3>
+            <h2> 0 </h2>
+          </div>
+        </NavLink>
       </div>
       <div className="div-card-finances">
-        <h3> Pedidos </h3>
-        <h2> 0 </h2>
+        <NavLink to={"/admin/pedidos"}>
+          <div className="link-financas">
+            <h3> Pedidos </h3>
+            <h2> {pedidos} </h2>
+          </div>
+        </NavLink>
       </div>
       <div className="div-card-finances">
-        <h3> Usuarios cadastrados </h3>
-        <h2> 0 </h2>
+        <NavLink to={"/admin/pedidos"}>
+          <div className="link-financas">
+            <h3> Usuarios cadastrados </h3>
+            <h2> 0 </h2>
+          </div>
+        </NavLink>
       </div>
     </div>
   );
--- src/pages/Finances/graph.jsx
+++ src/pages/Finances/graph.jsx
@@ -3,7 +3,7 @@ import { Chart } from "react-google-charts";
 import { useState, useEffect } from "react";
 import Api from "../../api/api";
 
-const data = [["Year", "Sales", "Quantidade: "]];
+const data = [["Dia", "Pedidos", "Quantidade: "]];
 
 let arr = [];
 
@@ -17,32 +17,40 @@ export default function App() {
   const [products, setProducts] = useState([]);
   {
     useEffect(() => {
-      // Tem que trocar a rota para os produtos que foram vendido
+      // Tem que trocar a rota para os produtos que foram vendidos
       Api.get(`api/public/products/pages/1`)
         .then((response) => {
           setProducts(response.data);
+        })
+        .then(() => {
+          for (let i = products; i !=[0]; i=5) {
+            console.log("breno")
+            i = products
+          }
           arr = products.map(function (obj) {
             return Object.keys(obj).map(function (key) {
               return obj[key];
             });
           });
         })
+
         .then(() => {
-          arr.forEach(function (number) {
+          arr.forEach((number) => {
             let arr1 = [number[1], number[2], number[4]];
             data.push(arr1);
           });
         })
         .catch(() => {
-          console.log("Deu tudo errado");
+          console.log("Deu ruim :( ");
         });
     }, []);
   }
+
   return (
     <Chart
       chartType="LineChart"
       width="100%"
-      height="400px"
+      height="90%"
       data={data}
       options={options}
     />
