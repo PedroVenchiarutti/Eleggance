51dcafe05a7223a059318ba958fd53bfaf82aee5
Douglas <douglasvitor.dev@gmail.com>
2022-08-29 11:34:43 -0300

    Componentizing some items and bug fixes

---
+++ src/components/Profile/common/TrashButton.jsx
@@ -0,0 +1,2 @@
+import './TrashButton.scss'
+export default ({onClick}) => <button className='profile-trash-button' onClick={onClick}><img src="/icons/icon-trash.svg" alt="Excluir" className="trashIcon" /></button>
