1f2d3b9ae91890015d679c5228561b612051ab5b
Douglas <douglasvitor.dev@gmail.com>
2022-08-31 11:50:51 -0300

    Some changes for admin authentication

--- src/pages/Admin/Login/Login.jsx
+++ src/pages/Admin/Login/Login.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from '../../../contexts/auth';
 import "./login.scss";
 
 export default function Login() {
-  const [adminLogin, setAdminLogin] = useState("");
-  const [adminSenha, setAdminSenha] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  function verificarLogin() {
-    if (adminLogin == "admin" && adminSenha == "admin") {
-    } else {
-      alert("Login bem-sucedido!");
-    }
-  }
+  const { login } = useContext(AuthContext);
+  const handleSubmit = () => login(email, password, '/admin/home');
 
   return (
     <div className="admin-login-container">
@@ -24,12 +21,12 @@ export default function Login() {
             <input
               type="text"
               name="Login"
-              value={adminLogin}
+              value={email}
               required
-              onChange={(e) => setAdminLogin(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <label htmlFor="Login" className="label-login">
-              <span className="content-login">Login</span>
+              <span className="content-login">E-mail</span>
             </label>
           </div>
           <div className="single-input">
@@ -37,16 +34,16 @@ export default function Login() {
               type="password"
               name="Password"
               required
-              value={adminSenha}
-              onChange={(e) => setAdminSenha(e.target.value)}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <label htmlFor="Password" className="label-password">
               <span className="content-password">Password</span>
             </label>
           </div>
         </form>
-        <div className="button-div">
-          <button onClick={verificarLogin}>Entrar</button>
+          <div className="button-div">
+            <button onClick={handleSubmit}>Entrar</button>
           </div>
       </div>
     </div>
