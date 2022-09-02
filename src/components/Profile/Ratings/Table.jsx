c043aae182d651878b74b4133347e0524034cf43
Douglas <douglasvitor.dev@gmail.com>
2022-09-02 12:51:24 -0300

    Fixing rating table css

--- src/components/Profile/Ratings/Table.jsx
+++ src/components/Profile/Ratings/Table.jsx
@@ -2,11 +2,9 @@ import { useContext } from 'react';
 import { RatingContext } from '../../../contexts/rating';
 
 import Table from '../../Table/Table';
-import SaleTag from '../common/SaleTag';
 import TrashButton from '../common/TrashButton';
 import Ratings from '../../Ratings'
 
-import './Table.scss'
 import { useFetch } from '../../../hooks/useFetch';
 
 export default () => <Table headerColumnsArray={[]} bodyObjectsArray={getRows()} />
@@ -21,20 +19,11 @@ const getRows = () => {
         return {
             key: rating.id,
             image: <td><img src={product.url_img} alt="Imagem do produto" /></td>,
-            productInfos: getProductInfos(product.name, false),
+            productName: <td>{product.name}</td>,
             rating: getProductRating(rating.stars, product.id),
             button: <td><TrashButton onClick={() => deleteRating(rating.id)} /></td>
         }
     })
 }
-
-
-const getProductInfos = (name, inSale) =>
-    <td className='products-cell'>
-        <div className='products-info'>
-            {name}
-            {inSale ? <SaleTag /> : ''}
-        </div>
-    </td>
 
 const getProductRating = (ratingStars, productId) => <td className='responsive-hide'><Ratings selectedStar={ratingStars} productId={productId} /></td>
