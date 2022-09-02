33f09f3591ebf0b20390540f8efd26ffa06e1d6d
BrenoOliveira <breno_2002_@outlook.com>
2022-09-02 08:42:44 -0300

    search filter

--- src/components/NabBarFinance/navBarFinances.jsx
+++ src/components/NabBarFinance/navBarFinances.jsx
@@ -1,113 +1,47 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
-import { useFetch } from "../../hooks/useFetch";
-import { NavLink } from "react-router-dom";
 
 import "../NabBarFinance/navBarFinances.scss";
 import "./navBarFinances.scss";
+import Search from "../Navbar/Search";
 export default (props) => {
   const { loginName } = useContext(AuthContext);
-  const { linkImg } = useContext(AuthContext);
-  const renderImage = () => {
-    if (authenticated) {
-      const getLocalStorage = localStorage.getItem("personal");
-      const img = JSON.parse(getLocalStorage);
-      if (img === null) {
-        return;
-      } else {
-        const linkImg = img[0].id.imgURL;
-        if (linkImg) {
-          return (
-            <button>
-              <img src={linkImg} className="imgUser" />
-            </button>
-          );
-        } else {
-          return (
-            <button>
-              <img src="/img/Frame.svg" />;
-            </button>
-          );
-        }
-      }
-    }
-  };
-  const { data } = useFetch(`api/public/products/pages/1`);
-
   const [busca, setBusca] = useState();
-  let objName = "";
-  const a = data.filter(function (obj) {
-    if (obj.name) {
-      objName = obj.name;
-      if (objName.startsWith(busca)) {
-        if (busca != "") {
-          return objName;
-        }
-      }
-    }
-  });
 
-  //========TOGGLE SEARCH ========//
-  function toggleSearch() {
-    var search = document.getElementById("search-products-finances");
-    if (search.style.display === "none") {
-      search.style.display = "block";
-    } else {
-      search.style.display = "none";
-    }
-  }
+
 
   return (
-    <div className="container-nav-finances">
-      <div className="nav-finances">
-        <div className="nav-finances-title">
-          <div className="finance-tittle">
-            <h2 className="h1-nav-finances">{props.name}</h2>
-          </div>
-          <input
-            id="search-products-finances"
-            className="search-products-finances"
-            onChange={(ev) => setBusca(ev.target.value)}
-          ></input>
-          <label
-            className="label-nav-finance"
-            htmlFor="search-products-finances"
-          >
-            <button onClick={toggleSearch} className="button-search">
-              <img className="icon-search" src="/icons/search.png" />
-            </button>
-          </label>
+    <div>
+      <div className="container-nav-finances">
+        <div className="nav-finances">
+          <div className="nav-finances-title">
+            <div className="finance-tittle">
+              <h2 className="h1-nav-finances">{props.name}</h2>
+            </div>
+            <input
+              id="search-products-finances"
+              className="search-products-finances"
+              onChange={(ev) => setBusca(ev.target.value.toUpperCase())}
+              
+            />
+            <label
+              className="label-nav-finance"
+              htmlFor="search-products-finances"
+            >
+              <button  className="button-search">
+                <img className="icon-search" src="/icons/search.png" />
+              </button>
+            </label>
 
-          <img className="icon-bell" src="/icons/notification.png" />
-        </div>
-        <div className="nav-finance-user">
-          <span>{loginName.toUpperCase()}</span>
+            <img className="icon-bell" src="/icons/notification.png" />
+          </div>
+          <div className="nav-finance-user">
+            <span>{loginName.toUpperCase()}</span>
+          </div>
         </div>
       </div>
-      <div className="search-produto">
-        {a.map((produto, keys) => {
-          return (
-            <NavLink key={produto.id} to={"/detalhes"}>
-              <div className="produto">
-                <div className="produto-photo">
-                  <img src={produto.url_img} />
-                </div>
-                <div className="container-produto">
-                  <p className="produto-name"> {produto.name}</p>
-                  <p className="produto-value">
-                    {" "}
-                    {"RS " +
-                      produto.value +
-                      ",00" +
-                      "  ou em 10x de R$ " +
-                      produto.value / 10 +
-                      ",00"}
-                  </p>
-                </div>
-              </div>
-            </NavLink>
-          );
-        })}
+      <div className="search-container">
+      <Search busca={busca} />
       </div>
     </div>
   );
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,52 +1,78 @@
 import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../contexts/auth";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./navbar.scss";
-import ModalUser from "../ModalUser/ModalUser";
+import { useFetch } from "../../hooks/useFetch";
+import Search from "./Search";
 export const Navbar = () => {
   const { authenticated, loginName } = useContext(AuthContext);
   const { userLogout } = useContext(AuthContext);
   const [active, setMode] = useState(false);
-  const [modalUser, setModalUser] = useState(false);
+
+  const [busca, setBusca] = useState();
 
   // Abrir/Fechar menu mobile Hamburguer
   const ToggleMode = () => {
     setMode(!active);
   };
 
-  const navigation = useNavigate();
-
-  //
-  const ToggleModalUser = () => {
-    setModalUser(!modalUser);
-    console.log("modalUser", modalUser);
-  };
-
-  // Pega a imagen do usuario e renderizar na navbar se nao tiver img pega a img padrao
+  // Pega a imagen do localStorage e renderizar na tela
   const renderImage = () => {
-    return (
-      <button>
-        <img src="./icons/userWhite.svg" alt="Foto usuario" />;
-      </button>
-    );
-  };
+    if (authenticated) {
+      const getLocalStorage = localStorage.getItem("personal");
+      const img = JSON.parse(getLocalStorage);
+      if (img === null) {
+        return;
+      } else {
+        const linkImg = img[0].id.imgURL;
+        if (linkImg) {
+          return (
+            <button>
+              <img
+                src={linkImg == " " ? "/img/Frame.svg" : linkImg}
+                alt="user"
+              />
 
+              {/* <img src={linkImg} className="imgUser" /> */}
+            </button>
+          );
+        } else {
+          return (
+            <button>
+              <img src="/img/Frame.svg" />;
+            </button>
+          );
+        }
+      }
+    }
+    console.log(linkImg);
+  };
   //verificar a tela do usuario e renderizar o botao de logout e se ele estiver logado ou nao
   const userLogged = () => {
     const height = window.screen.height;
     const width = window.screen.width;
-
     if (height >= 600 && width >= 600 && authenticated) {
-      // renderizando Desktop
       return (
+        // <<<<<<< HEAD=
+        //         <div className="navbar-user-logged-mobile">
+        //           <div className="navbar-user-logged-name-mobile">
+        //             <button>
+        //               <img src="/img/Frame.svg" className="svgUser-mobile" />
+        //             </button>
+        //             <button className="buttonCart">
+        //               <Link to="/carrinho">
+        //                 <img src="/icons/shoppingCart.svg" className="svgCart" />
+        //               </Link>
+        //             </button>
+        //           </div>
+        // =======
+
         <div className="navbar-user-logged">
           <div className="navbar-user-logged-name">
             {renderImage()}
             <div className="navbar-user-name-logout">
               <h4>
-                <span onClick={() => navigation("/perfil")}>
-                  {loginName.toUpperCase()}
-                </span>
+                <span>{loginName.toUpperCase()}</span>
               </h4>
               <button onClick={userLogout}>Sair</button>
             </div>
@@ -56,6 +82,7 @@ export const Navbar = () => {
               <img src="/icons/shoppingCart.svg" className="svgCart" />
             </Link>
           </button>
+          {/* >>>>>>> fab52d74559bccac682280cb08820324e4906052 */}
         </div>
       );
     } else if (height >= 600 && width >= 600 && !authenticated) {
@@ -71,7 +98,7 @@ export const Navbar = () => {
           </div>
           <button className="buttonCart">
             <Link to="/carrinho">
-              <img src="/icons/shoppingCart.svg" className="svgCart" />
+              <img src="/icons/shoppingCart.svg" className="svgCart-mobile" />
             </Link>
           </button>
         </div>
@@ -81,62 +108,70 @@ export const Navbar = () => {
       return (
         <div className="navbar-user-logged-mobile">
           <div className="navbar-user-logged-name-mobile">
-            <button className="buttonFrame" onClick={ToggleModalUser}>
+            <button className="buttonFrame">
               <img src="/img/Frame.svg" className="svgUser-mobile" />
             </button>
-            {modalUser ? <ModalUser /> : null}
-            {/*   <button className="buttonCart">
+            <button className="buttonCart">
               <Link to="/carrinho">
-                <img src="/icons/shoppingCart.svg" className="svgCart-mobile" />
+                <img src="/icons/shoppingCart.svg" className="svgCart" />
               </Link>
-            </button> */}
+            </button>
           </div>
         </div>
       );
     }
   };
   return (
-    <div className="navbar-container">
-      <nav>
-        <div className="logoInSearch">
-          <Link to="/">
-            <img src="/logo.png" className="logo-navbar" />
-          </Link>
-          <div className="inputSearch">
-            <div className="inputList">
-              <input type="text" id="txtBusca" placeholder="Buscar..." />
-              <div className={active ? "menu menuOpen" : "menu menuClose"}>
-                <div className="list-container">
-                  <div className="list">
-                    <ul>
-                      <li>
-                        <Link to="/">Home</Link>
-                      </li>
-                      <li>
-                        <Link to="/produtos">Produtos</Link>
-                      </li>
-                      <li>
-                        <Link to="/contato">Contato</Link>
-                      </li>
-                    </ul>
+    <div>
+      <div className="navbar-container">
+        <nav>
+          <div className="logoInSearch">
+            <Link to="/">
+              <img src="/logo.png" />
+            </Link>
+            <div className="inputSearch">
+              <div className="inputList">
+                <input
+                  type="text"
+                  id="txtBusca"
+                  placeholder="Buscar..."
+                  onChange={(ev) => setBusca(ev.target.value.toUpperCase())}
+                />
+                <div className={active ? "menu menuOpen" : "menu menuClose"}>
+                  <div className="list-container">
+                    <div className="list">
+                      <ul>
+                        <li>
+                          <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                          <Link to="/produtos">Produtos</Link>
+                        </li>
+                        <li>
+                          <Link to="/contato">Contato</Link>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
+              <button>
+                <img src="/icons/iconmonstr-search-thin.svg" />
+              </button>
             </div>
-            <button>
-              <img src="/icons/iconmonstr-search-thin.svg" />
-            </button>
           </div>
-        </div>
-        {/* menu hamburgue */}
-        {userLogged()}
-        <div
-          className={active ? "icon iconActive" : "icon"}
-          onClick={ToggleMode}
-        >
-          <div className="hamburguer hamburguerIcon"></div>
-        </div>
-      </nav>
+          {/* menu hamburgue */}
+          {userLogged()}
+          <div
+            className={active ? "icon iconActive" : "icon"}
+            onClick={ToggleMode}
+          >
+            <div className="hamburguer hamburguerIcon"></div>
+          </div>
+        </nav>
+  
+      </div>
+      <Search busca={busca} />
     </div>
   );
 };
