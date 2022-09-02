906b3e5f7573f4a6003e8ddd42a8d8eb7851ed01
Douglas <douglasvitor.dev@gmail.com>
2022-09-02 10:57:33 -0300

    Conditional for when create/update review

--- src/contexts/rating.jsx
+++ src/contexts/rating.jsx
@@ -7,15 +7,19 @@ const BASE_URL = "api/protected/client/reviews";
 export const RatingContext = createContext();
 export const RatingProvider = ({ children }) => {
     const userId = 1; // CHANGE TO AUTHENTICATED USER ID
+    
+    const { data } = useFetch(`${BASE_URL}/${userId}`)
 
     const saveRating = (stars, productId) => {
         const review = {
+            ...data.find(item => item.user_id == userId),
             stars: stars / 20,
             user_id: userId,
             product_id: productId
         }
 
-        Api.post(BASE_URL, review).then(() => alert('Agradecemos a sua avaliação.'));
+        if (review.id) Api.put(`${BASE_URL}/${review.id}`, review).then (() => alert("Avaliação atualizada com sucesso"));
+        else Api.post(BASE_URL, review).then(() => alert('Agradecemos a sua avaliação.'));
     }
 
     const deleteRating = ratingId => Api.delete(`api/protected/client/reviews/${ratingId}`).then(() => {
@@ -23,7 +27,6 @@ export const RatingProvider = ({ children }) => {
         window.location.reload();
     })
 
-    const { data } = useFetch(`${BASE_URL}/${userId}`)
     const state = { ratings: data, saveRating, deleteRating }
     return <RatingContext.Provider value={state}>{children}</RatingContext.Provider>
 }
