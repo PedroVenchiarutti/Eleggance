f7c931d8c8686fb0cfcad4466a5be2fbbc868f33
Douglas <douglasvitor.dev@gmail.com>
2022-09-05 09:30:02 -0300

    Improvements in rating funcionality

--- src/components/Ratings/index.jsx
+++ src/components/Ratings/index.jsx
@@ -1,22 +1,19 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { RatingContext } from '../../contexts/rating';
 
 import { Rating } from 'react-simple-star-rating'
 
 import './index.scss'
 
-export default ({ productId, selectedStar }) => {
-    const { saveRating } = useContext(RatingContext);
-    const [rating, setRating] = useState(selectedStar ? selectedStar * 20 : 0);
-
-    const handleChange = rate => {
-        saveRating(rate, productId);
-        setRating(rate);
-    }
-
+export default ({ productId, selectedStar}) => {
+    const { getSelectedRating, selectedRating, saveRating } = useContext(RatingContext);
+    getSelectedRating(productId);
+    const handleChange = rate => saveRating(rate, productId)
+    
+    const stars = selectedStar ? selectedStar * 20 : selectedRating.stars * 20;
     return (
         <div className="rating-container">
-            <Rating onClick={handleChange} initialValue={rating} ratingValue={rating} />
+            <Rating onClick={handleChange} initialValue={stars} ratingValue={stars} />
         </div>
     )
 }
--- src/contexts/rating.jsx
+++ src/contexts/rating.jsx
@@ -1,25 +1,45 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { createContext } from "react";
-import {  useFetch } from '../hooks/useFetch';
+
 import Api from "../api/api";
+import { useFetch } from '../hooks/useFetch';
 
 const BASE_URL = "api/protected/client/reviews";
 
 export const RatingContext = createContext();
 export const RatingProvider = ({ children }) => {
     const userId = 1; // CHANGE TO AUTHENTICATED USER ID
-    
-    const { data } = useFetch(`${BASE_URL}/${userId}`)
+
+    const { data } = useFetch(`${BASE_URL}/${userId}`);
+
+    const [selectedRating, setSelectedRating] = useState({
+        user_id: userId,
+        stars: 0
+    });
+    const getSelectedRating = productId => {
+        const { data } = useFetch(`${BASE_URL}/${userId}/${productId}`);
+        if (data.stars) setSelectedRating(data);
+    }
 
     const saveRating = (stars, productId) => {
         const review = {
-            ...data.find(item => item.user_id == userId),
+            ...data.find(item => item.product_id == productId),
             stars: stars / 20,
             user_id: userId,
             product_id: productId
         }
 
-        if (review.id) Api.put(`${BASE_URL}/${review.id}`, review).then (() => alert("Avaliação atualizada com sucesso"));
-        else Api.post(BASE_URL, review).then(() => alert('Agradecemos a sua avaliação.'));
+        const onSuccess = message => {
+            alert(message);
+            data.push(review);
+        }
+
+        if (review.id) Api.put(`${BASE_URL}/${review.id}`, review).then(() => onSuccess("Avaliação atualizada com sucesso"));
+        else Api.post(BASE_URL, review).then(resp => {
+            review.id = resp.data.id;
+            onSuccess("Agradecemos a sua avaliação.");
+        });
     }
 
     const deleteRating = ratingId => Api.delete(`api/protected/client/reviews/${ratingId}`).then(() => {
@@ -27,6 +47,6 @@ export const RatingProvider = ({ children }) => {
         window.location.reload();
     })
 
-    const state = { ratings: data, saveRating, deleteRating }
+    const state = { getSelectedRating, selectedRating, ratings: data, saveRating, deleteRating }
     return <RatingContext.Provider value={state}>{children}</RatingContext.Provider>
 }
