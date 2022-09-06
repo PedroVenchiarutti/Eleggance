c3871e22196f30a09b9e0e14e1dcf0d6f6527231
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-09-06 12:25:45 -0300

    finalizing pagination and adjusts in footer

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -4,31 +4,23 @@ import "./Footer.scss";
 const Footer = (props) => {
   return (
     <footer className="footer">
-      <div className="footer__container">
-        <div className="footer__container__left">
-          <div className="footer__container__left__logo">
-            <div className="footer_logo">
-              <img src="/logo.png" />
-            </div>
-            <div className="footer_content_img">
-              <img src="/img/teste.png" />
-            </div>
-            <div className="footer_img">
-              <div className="icons">
-                <a href="#">
-                  <img src="/icons/instagram.svg" />
-                </a>
-                <a href="#">
-                  <img src="/icons/facebook.svg" />
-                </a>
-                <a href="#">
-                  <img src="/icons/whatsApp.svg" alt="" />
-                </a>
-              </div>
-              <p>Nos sigas nas redes sociais</p>
-            </div>
-          </div>
+      <img className="footer_logo" src="/logo.png" />
+      <div className="footer_content_img">
+        <img src="/img/teste.png" />
+      </div>
+      <div className="footer_img">
+        <div className="icons">
+          <a href="#">
+            <img src="/icons/instagram.svg" />
+          </a>
+          <a href="#">
+            <img src="/icons/facebook.svg" />
+          </a>
+          <a href="#">
+            <img src="/icons/whatsApp.svg" alt="" />
+          </a>
         </div>
+        <p>Nos sigas nas redes sociais</p>
       </div>
     </footer>
   );
