9295b0200065d79db24fa08447e0d943a972d78f
Smooke09 <pedrolucas1014zz@gmail.com>
2022-09-19 09:06:44 -0300

    ajust scss homepage

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -12,47 +12,49 @@ const Carrousel = ({ products, title }) => {
   if (!products) return;
   return (
     <div className="container-promo">
-      <div className="swiperSlidePromo">
-        <h1 className="blog-title">{title}</h1>
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={10}
-          pagination={{
-            clickable: true,
-          }}
-          breakpoints={{
-            640: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 4,
-              spaceBetween: 40,
-            },
-            1024: {
-              slidesPerView: 5,
-              spaceBetween: 50,
-            },
-          }}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          {products.map((product, index) => {
-            return (
-              <SwiperSlide key={index} className="swiper-container">
-                <Link
-                  to={`/detalhes/${product.id}`}
-                  style={{ textDecoration: "none", color: "black" }}
-                  onClick={() => {
-                    location.reload();
-                  }}
-                >
-                  <Card product={product} />
-                </Link>
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
+      <div className="content-promo">
+        <div className="swiperSlidePromo">
+          <h1 className="blog-title">{title}</h1>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={10}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 5,
+                spaceBetween: 50,
+              },
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            {products.map((product, index) => {
+              return (
+                <SwiperSlide key={index} className="swiper-container">
+                  <Link
+                    to={`/detalhes/${product.id}`}
+                    style={{ textDecoration: "none", color: "black" }}
+                    onClick={() => {
+                      location.reload();
+                    }}
+                  >
+                    <Card product={product} />
+                  </Link>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
       </div>
     </div>
   );
